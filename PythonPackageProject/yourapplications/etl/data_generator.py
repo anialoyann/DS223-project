e87440afd0db719516c3ab25225b981aaf627962
eohanjanyan <elina.ohanjanyan@gmail.com>
2024-11-15 23:36:08 +0400

    database design and pushing to pgadmin, will add the methods later

--- PythonPackageProject/yourapplications/etl/data_generator.py
+++ PythonPackageProject/yourapplications/etl/data_generator.py
@@ -7,23 +7,26 @@ import os
 from loguru import logger
 fake=Faker()
 
+random.seed(10)
+
 real_films = [
-    {"product_id": 1, "name": "Inception", "release_year": 2010, "rating": 8.8},
-    {"product_id": 2, "name": "The Godfather", "release_year": 1972, "rating": 9.2},
-    {"product_id": 3, "name": "The Dark Knight", "release_year": 2008, "rating": 9.0},
-    {"product_id": 4, "name": "Pulp Fiction", "release_year": 1994, "rating": 8.9},
-    {"product_id": 5, "name": "Forrest Gump", "release_year": 1994, "rating": 8.8},
-    {"product_id": 6, "name": "The Matrix", "release_year": 1999, "rating": 8.7},
-    {"product_id": 7, "name": "Fight Club", "release_year": 1999, "rating": 8.8},
-    {"product_id": 8, "name": "Interstellar", "release_year": 2014, "rating": 8.6},
-    {"product_id": 9, "name": "The Lord of the Rings: The Return of the King", "release_year": 2003, "rating": 8.9},
-    {"product_id": 10, "name": "The Shawshank Redemption", "release_year": 1994, "rating": 9.3},
+    {"movie_id": 1, "movie_name": "Inception", "release_year": 2010, "movie_rating": 8.8, "movie_genre": "Sci-Fi", "movie_duration": 148},
+    {"movie_id": 2, "movie_name": "The Godfather", "release_year": 1972, "movie_rating": 9.2, "movie_genre": "Crime", "movie_duration": 175},
+    {"movie_id": 3, "movie_name": "The Dark Knight", "release_year": 2008, "movie_rating": 9.0, "movie_genre": "Action", "movie_duration": 152},
+    {"movie_id": 4, "movie_name": "Pulp Fiction", "release_year": 1994, "movie_rating": 8.9, "movie_genre": "Drama", "movie_duration": 154},
+    {"movie_id": 5, "movie_name": "Forrest Gump", "release_year": 1994, "movie_rating": 8.8, "movie_genre": "Comedy", "movie_duration": 142},
+    {"movie_id": 6, "movie_name": "The Matrix", "release_year": 1999, "movie_rating": 8.7, "movie_genre": "Sci-Fi", "movie_duration": 136},
+    {"movie_id": 7, "movie_name": "Fight Club", "release_year": 1999, "movie_rating": 8.8, "movie_genre": "Drama", "movie_duration": 139},
+    {"movie_id": 8, "movie_name": "Interstellar", "release_year": 2014, "movie_rating": 8.6, "movie_genre": "Sci-Fi", "movie_duration": 169},
+    {"movie_id": 9, "movie_name": "The Lord of the Rings: The Return of the King", "release_year": 2003, "movie_rating": 8.9, "movie_genre": "Fantasy", "movie_duration": 201},
+    {"movie_id": 10, "movie_name": "The Shawshank Redemption", "release_year": 1994, "movie_rating": 9.3, "movie_genre": "Drama", "movie_duration": 142},
 ]
-def generate_product(product_id):
-    if 1 <= product_id <= len(real_films):
-        return real_films[product_id - 1]
+
+def generate_movie(movie_id):
+    if 1 <= movie_id <= len(real_films):
+        return real_films[movie_id - 1]
     else:
-        raise ValueError("Invalid product_id: no predefined product exists with this ID.")
+        raise ValueError("Invalid movie_id: no predefined product exists with this ID.")
 
 def generate_subscription(subscription_id):
     predefined_subscriptions = [
@@ -38,7 +41,27 @@ def generate_subscription(subscription_id):
     else:
         raise ValueError("Invalid subscription_id: no predefined subscription exists with this ID.")
 
+def generate_segment(segment_id):
+    predefined_segments = [
+        {"segment_id": 1, 
+         "segment_name": "Lost Cause",
+         "segment_description": "Customers with minimal engagement and low value."},
+        {"segment_id": 2, 
+         "segment_name": "Vulnerable Customers", 
+         "segment_description": "At-risk customers who can be retained."},
+        {"segment_id": 3, 
+         "segment_name": "Free Riders", 
+         "segment_description": "Customers who use free services with low revenue contribution."},
+        {"segment_id": 4, 
+         "segment_name": "Star Customers", 
+         "segment_description": "High-value customers who are highly engaged and loyal."},
+    ]
 
+    if 1 <= segment_id <= len(predefined_segments):
+        return predefined_segments[segment_id - 1]
+    else:
+        raise ValueError("Invalid subscription_id: no predefined subscription exists with this ID.")
+    
 def generate_customer(customer_id, subscription_id):
     created_at = fake.date_time_this_decade()
     updated_at = fake.date_time_between(start_date=created_at, end_date="now")
@@ -52,15 +75,16 @@ def generate_customer(customer_id, subscription_id):
         "updated_at": updated_at,
     }
 
-def generate_engagement(engagement_id, customer_id, product_id):
+def generate_engagement(engagement_id, customer_id, movie_id):
     return {
         "engagement_id": engagement_id,
         "customer_id": customer_id,
-        "product_id": product_id,
+        "movie_id": movie_id,
         "session_duration": random.randint(1, 360),
         "session_date": fake.date_this_year(),
         "device_type": fake.random_element(elements=["Mobile", "Desktop", "Tablet"]),
-        "watched_fully": fake.boolean(chance_of_getting_true=60)
+        "watched_fully": fake.boolean(chance_of_getting_true=60),
+        "like_status": fake.random_element(elements=["Liked", "Disliked", "No Action"]),
     }
 
 
@@ -68,24 +92,32 @@ def generate_ab_test(ab_test_id):
     predefined_types = [
         {
             "ab_test_id": 1,
+            "goal": "subscription",
+            "targeting": "by genre",
             "test_variant": 1,
-            "test_goal": "Streaming"
+            "text_skeleton": "Unlock exclusive access to top-rated (insert_genre) movies! Subscribe now for just (insert_price) ."
         },
         {
             "ab_test_id": 2,
+            "goal": "subscription",
+            "targeting": "subscription sale",
             "test_variant": 2,
-            "test_goal": "Streaming"
+            "text_skeleton": "Special deal for (insert_subscription_name) subscribers: Only (insert price) for a limited time!"
         },
         {
             "ab_test_id": 3,
+            "goal": "engagement",
+            "targeting": "by movie",
             "test_variant": 1,
-            "test_goal": "Subscription"
+            "text_skeleton": "Have you seen (insert_movies)? Don't missout! Watch now and enjoy the experience."
         },
         {
             "ab_test_id": 4,
+            "goal": "engagement",
+            "targeting": "by genre",
             "test_variant": 2,
-            "test_goal": "Subscription"
-        },
+            "text_skeleton": "Experience the magic of (insert_genre) likenever before. Click here and start watching!"
+        }
     ]
 
     if 1 <= ab_test_id <= len(predefined_types):
--- PythonPackageProject/yourapplications/etl/models.py
+++ PythonPackageProject/yourapplications/etl/models.py
@@ -16,14 +16,15 @@ class Subscription(Base):
     subscription_name = Column(String)
     price = Column(Integer)
 
-class Product(Base):
-    __tablename__ = "products"
+class Movie(Base):
+    __tablename__ = "movies"
 
-    product_id = Column(Integer, primary_key=True)
-    name = Column(String)
+    movie_id = Column(Integer, primary_key=True)
+    movie_name = Column(String)
     release_year = Column(Integer)
-    rating = Column(Float)
-    
+    movie_duration = Column(Integer)
+    movie_rating = Column(Float)
+    movie_genre = Column(String)
 
 class Customer(Base):
     __tablename__ = "customers"
@@ -38,28 +39,58 @@ class Customer(Base):
 
     subscription = relationship("Subscription")
 
+class Segment(Base):
+    __tablename__ = "segments"
+
+    segment_id = Column(Integer, primary_key=True)
+    segment_name = Column(String)
+    segment_description = Column(Text)
+
+class CustomerSegment(Base):
+    __tablename__ = "customer_segments"
+
+    customer_segment_id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey("customers.customer_id"))
+    segment_id = Column(Integer, ForeignKey("segments.segment_id"))
+
+    customer = relationship("Customer")
+    segment = relationship("Segment")
 
 class Engagement(Base):
     __tablename__ = "engagements"
     
     engagement_id = Column(Integer, primary_key=True)
     customer_id = Column(Integer, ForeignKey("customers.customer_id"))
-    product_id =  Column(Integer, ForeignKey("products.product_id"))
+    movie_id =  Column(Integer, ForeignKey("movies.movie_id"))
     session_duration = Column(Integer)
     session_date = Column(DateTime)
     device_type = Column(String)
     watched_fully = Column(Boolean)
+    like_status = Column(String)
 
     customer = relationship("Customer")
-    prouct = relationship("Product")
+    movie = relationship("Movie")
 
 class ABTest(Base):
     __tablename__ = "ab_tests"
     
     ab_test_id = Column(Integer, primary_key=True)
+    goal = Column(String)
+    targeting = Column(String)
     test_variant = Column(Integer)
-    test_goal = Column(String)
+    text_skeleton = Column(Text)
 
+class ABTest_Result(Base):
+    __tablename__ = "ab_test_results"
+
+    result_id = Column(Integer, primary_key=True)
+    ab_test_id = Column(Integer, ForeignKey("ab_tests.ab_test_id"))
+    customer_id = Column(Integer, ForeignKey("customers.customer_id"))
+    clicked_link = Column(Boolean)
+    time_spent_seconds = Column(Integer)
+
+    customer = relationship("Customer")
+    abtest = relationship("ABTest")
 
 Base.metadata.create_all(engine)
 
